60dd71aa51b2edeb8fd076077c47ec24ef1c9445
ganeshjangid <webdeveloper.ganesh@gmail.com>
2019-06-01 13:53:08 +0530

    test

---
+++ clear_concept/module_import.js
@@ -0,0 +1,10 @@
+/* let add=(a,b)=> `this is ${a} and ${b}`;
+
+module.exports=add;
+ */
+
+/* exports.add = (a, b) => `this is ${a} and ${b}`;
+ */
+
+
+ module.exports=(a,b)=> `this is ${a} and ${b}`;
---
+++ clear_concept/module_test.js
@@ -0,0 +1,6 @@
+const module_test=require('./module_import');
+
+//console.log(module_test(10,20));
+//console.log(module_test.add(10,20));
+console.log(module_test(10, 20));
+
